5cc6adf0cba3e59172d2e8f4318aaabb1dbad2ce
Nick McKinney <nmckinney@breakpoint-labs.com>
2021-06-07 13:23:27 -0400

    Patch account controller to fix auth redirect loop

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'redmine'
-require 'ext/account_controller'
+require 'redmine_ssl_auth_cn/account_controller_patch'
 
 Redmine::Plugin.register :redmine_ssl_auth_cn do
   name 'Redmine SSL Auth CN plugin'
@@ -7,5 +7,10 @@ Redmine::Plugin.register :redmine_ssl_auth_cn do
   description 'Enable authentication using SSL client certificates where username=[certificate common name].  Forked from code by Steve Simpson'
   url 'https://github.com/NullOranje/redmine_ssl_auth_cn'
   author_url 'http://github.com/NullOranje/'  
-  version '1.0.2'
+  version '1.0.4'
+  settings default: {'allow_username_password' => 'true', 'http_header' => "X-Proxy-Forwarded-CN"}, partial: 'settings/redmine_ssl_auth_cn'
+end
+
+Rails.configuration.to_prepare do
+  AccountController.prepend(RedmineSSLAuthCn::AccountControllerPatch)
 end
